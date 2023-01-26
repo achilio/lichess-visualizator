a809b56289cbac7b7cae7d2f5c12e0bc673a6ede
estebgonza <esteban.gonzalez@achilio.com>
2023-01-26 20:51:48 +0100

    use node-fetch and ndjson

--- src/composables/games.ts
+++ src/composables/games.ts
@@ -1,25 +1,5 @@
-const ndjson = require('ndjson')
-
-// export const readStream = (processLine: any) => (response: Response) => {
-//   const matcher = /\r?\n/
-//   const decoder = new TextDecoder()
-//   let buf = ''
-//   return new Promise<void>((resolve, fail) => {
-//     response.body?.on('data', (v: any) => {
-//       const chunk = decoder.decode(v, { stream: true })
-//       buf += chunk
-
-//       const parts = buf.split(matcher)
-//       buf = parts.pop() || ''
-//       for (const i of parts.filter((p) => p)) processLine(JSON.parse(i))
-//     })
-//     response.body?.on('end', () => {
-//       if (buf.length > 0) processLine(JSON.parse(buf))
-//       resolve()
-//     })
-//     response.body?.on('error', fail)
-//   })
-// }
+import ndjson from 'ndjson'
+import fetch from 'node-fetch'
 
 export const fetchGames = async (username: string, max: number) => {
   console.log(`Fetching games for ${username}...`)
@@ -32,25 +12,7 @@ export const fetchGames = async (username: string, max: number) => {
       },
     }
   )
-  console.log(stream)
-  const reader = stream.body?.pipeThrough(ndjson.parse())
-  console.log(reader)
-  // .then((res) => {
-  //   console.log(res.body)
-  //   return ndjson(res.body)
-  // })
-  // .then((stream) => {
-  //   const reader = stream.getReader()
-  //   let read: any
-  //   reader.read().then(
-  //     (read = (result: any) => {
-  //       if (result.done) {
-  //         reader.releaseLock()
-  //         return
-  //       }
-  //       console.log(result.value)
-  //       reader.read().then(read)
-  //     })
-  //   )
-  // })
+  const reader = stream.body?.pipe(ndjson.parse()).on('data', (data: any) => {
+    console.log(data)
+  })
 }
