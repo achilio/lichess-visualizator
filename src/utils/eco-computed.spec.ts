5080b8a024e57dc411949fa32c3812e07cb1b33c
NicoGGG <nicolas.guelfi@achilio.com>
2023-02-05 00:59:58 +0100

    feat(eco-computed): removed recomputeTrigger

--- src/utils/eco-computed.spec.ts
+++ src/utils/eco-computed.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { ecoComputed } from './eco-computed'
 describe('Eco-computed', () => {
   it('Single reactive property', async () => {
@@ -11,9 +11,10 @@ describe('Eco-computed', () => {
     expect(counter.value).toBe(1)
     val.value++
     expect(counter.value).toBe(1)
-    await wait(100)
+    await wait(110)
+    val.value++
     // After waiting, the value should have changed
-    expect(counter.value).toBe(3)
+    expect(counter.value).toBe(4)
   })
   it('Multiple reactive properties', async () => {
     const val1 = ref(1)
@@ -35,6 +36,7 @@ describe('Eco-computed', () => {
   it('Initial trigger is effective', async () => {
     const val = ref(1)
     const counter = ecoComputed(() => val.value, 100)
+    expect(counter.value).toBe(1)
     val.value++
     expect(counter.value).toBe(1)
     val.value++
@@ -53,35 +55,43 @@ describe('Eco-computed', () => {
     val.value++
     val.value++
     expect(counter.value).toBe(1)
-    await wait(100)
+    await wait(110)
     // After waiting, the value should have changed
     expect(counter.value).toBe(6)
   })
-  it('Check number of calls in the computed', async () => {
-    const val = ref(1)
+  it('Check number of calls in the eco computed', async () => {
+    const val = ref<number>(0)
     let computedCalls = 0
     const counter = ecoComputed(() => {
+      setTimeout(() => {
+        console.log('computedCalls', computedCalls)
+      }, 100)
       computedCalls++
       return val.value
     }, 100)
-    expect(counter.value).toBe(1)
-    expect(computedCalls).toBe(1)
-    val.value++
-    expect(counter.value).toBe(1)
-    expect(computedCalls).toBe(1)
-    await wait(100)
-    // After waiting, the value should have changed
-    expect(counter.value).toBe(2)
-    expect(computedCalls).toBe(2)
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 10; i++) {
       val.value++
+      expect(counter.value).not.toBeNull()
     }
-    expect(counter.value).toBe(2)
-    expect(computedCalls).toBe(2)
     await wait(100)
-    // After waiting, the value should have changed
-    expect(counter.value).toBe(102)
-    expect(computedCalls).toBe(3)
+    expect(computedCalls).toBe(1)
+  })
+  it('Check number of calls in the native computed', async () => {
+    const val = ref<number>(0)
+    let computedCalls = 0
+    const counter = computed(() => {
+      setTimeout(() => {
+        console.log('computedCalls', computedCalls)
+      }, 100)
+      computedCalls++
+      return val.value
+    })
+    for (let i = 0; i < 10; i++) {
+      val.value++
+      expect(counter.value).not.toBeNull()
+      expect(computedCalls).toBe(i + 1)
+    }
+    await wait(100)
   })
 })
 
--- src/utils/eco-computed.ts
+++ src/utils/eco-computed.ts
@@ -1,4 +1,4 @@
-import { computed, ComputedRef, ref } from 'vue'
+import { computed, ComputedRef, Ref, ref } from 'vue'
 
 /**
  * A computed that will not recompute too often
@@ -7,31 +7,32 @@ import { computed, ComputedRef, ref } from 'vue'
  * @returns A computed that will not recompute too often
  */
 export function ecoComputed<T>(fn: () => T, minDelay: number): ComputedRef<T> {
-  let lastComputedValue: T = fn()
-  let lastComputedAt = Date.now()
-  let recomputeTrigger: NodeJS.Timeout | null
-  const triggerRef = ref(0)
+  let lastComputedValue: T
+  let lastComputedAt = 0
+  let lock: boolean = false
+  const triggerRef: Ref<number> = ref(0)
   return computed(() => {
-    // Trigger a re-computation when the triggerRef changes
     triggerRef.value
+    // Trigger a re-computation when the triggerRef changes
     // If the last computation was too recent, schedule a new one
     const nextComputedAvailableAt = lastComputedAt + minDelay
     // If the next computation is scheduled in the future, return the cached value
     // and schedule a new computation
-    if (nextComputedAvailableAt <= Date.now()) {
+    if (nextComputedAvailableAt <= Date.now() && !lock) {
+      lock = true
       // Recompute is allowed, clear the scheduled recompute
       lastComputedValue = fn()
       // Update the last computed time after the value has been computed
       lastComputedAt = Date.now()
-    } else if (!recomputeTrigger) {
+      lock = false
+    } else {
       // The delay to wait before recomputing
       const delayNextCompute = nextComputedAvailableAt - Date.now()
       // Schedule a new computation
-      recomputeTrigger = setTimeout(
+      setTimeout(
         // Retrigger the computed when the timeout is over and reset the timeout
         () => {
           triggerRef.value++
-          recomputeTrigger = null
         },
         delayNextCompute
       )
