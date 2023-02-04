be139232a79d9db7241478ff3064f07609244af9
estebgonza <esteban.gonzalez@achilio.com>
2023-02-04 16:08:39 +0100

    remove comments

--- src/utils/eco-computed.ts
+++ src/utils/eco-computed.ts
@@ -19,16 +19,13 @@ export function ecoComputed<T>(fn: () => T, minDelay: number): ComputedRef<T> {
     // If the next computation is scheduled in the future, return the cached value
     // and schedule a new computation
     if (nextComputedAvailableAt <= Date.now()) {
-      console.debug('Eco-computed: recompute')
       // Recompute is allowed, clear the scheduled recompute
       lastComputedValue = fn()
       // Update the last computed time after the value has been computed
       lastComputedAt = Date.now()
     } else if (!recomputeTrigger) {
-      console.debug('Eco-computed: recompute scheduled')
       // The delay to wait before recomputing
       const delayNextCompute = nextComputedAvailableAt - Date.now()
-      console.debug('Eco-computed: recompute scheduled in', delayNextCompute)
       // Schedule a new computation
       recomputeTrigger = setTimeout(
         // Retrigger the computed when the timeout is over and reset the timeout
@@ -38,12 +35,8 @@ export function ecoComputed<T>(fn: () => T, minDelay: number): ComputedRef<T> {
         },
         delayNextCompute
       )
-      console.debug('Eco-computed: recompute scheduled in', delayNextCompute)
-    } else {
-      console.debug('Eco-computed: recompute already scheduled')
     }
     // Return the last computed value (either from the cache or from the function)
-    console.debug('Eco-computed: return value: ', lastComputedValue)
     return lastComputedValue
   })
 }
